9ed0a6d91d771bb0693b6c754622fa797f088d2d
Tung Du <dinhtungdu@gmail.com>
2023-10-13 13:37:05 +0700

    initial sofle keymap

--- keyboards/sofle/keymaps/default/keymap.c
+++ keyboards/sofle/keymaps/default/keymap.c
@@ -38,11 +38,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
-  KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
-  KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-                 KC_LGUI,KC_LALT,KC_LCTL, MO(_LOWER), KC_ENT,      KC_SPC,  MO(_RAISE), KC_RCTL, KC_RALT, KC_RGUI
+  KC_GRV,  KC_1,          KC_2,           KC_3,           KC_4,           KC_5,                         KC_6,    KC_7,           KC_8,           KC_9,           KC_0,         KC_MINS,
+  KC_TAB,  KC_Q,          KC_W,           KC_E,           KC_R,           KC_T,                         KC_Y,    KC_U,           KC_I,           KC_O,           KC_P,         KC_LBRC,
+  KC_ESC,  KC_A,          KC_S,           KC_D,           KC_F,           KC_G,                         KC_H,    KC_J,           KC_K,           KC_L,           KC_SCLN,      KC_QUOT,
+  KC_LSFT, KC_Z,          KC_X,           KC_C,           KC_V,           KC_B, KC_MUTE,       XXXXXXX, KC_N,    KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,      KC_RSFT,
+           LSG_T(KC_F15), LALT_T(KC_F16) ,LCTL_T(KC_F17), LGUI_T(KC_F18), LT(_LOWER,KC_F19), LT(_LOWER, KC_F20), RGUI_T(KC_F21), RCTL_T(KC_F22), RALT_T(KC_F23), C_S_T(KC_F24)
 ),
 /*
  * COLEMAK
@@ -82,11 +82,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_LOWER] = LAYOUT(
-  _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
-  KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_F12,
-  _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-  _______,  KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, _______,       _______, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, _______,
-                       _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,
+  _______, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_4, KC_KP_5,                         KC_KP_6, KC_KP_7, KC_KP_8, KC_KP_9,  KC_KP_0, KC_F12,
+  KC_CAPS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,                         KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,
+  _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, _______,       _______, _______, _______, _______, _______,  _______, _______,
+                    _______, _______, _______, _______, KC_SPC,        KC_BSPC, _______, _______, _______, _______
 ),
 /* RAISE
  * ,----------------------------------------.                    ,-----------------------------------------.
