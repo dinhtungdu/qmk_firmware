630bb2a987a9a92b19f435dab1db31ef1efc484f
Tung Du <dinhtungdu@gmail.com>
2023-10-14 21:30:13 +0700

    update keymap, full mod tap with F keys

--- keyboards/sofle/keymaps/default/keymap.c
+++ keyboards/sofle/keymaps/default/keymap.c
@@ -38,11 +38,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT(
-  KC_GRV,  KC_1,          KC_2,           KC_3,           KC_4,           KC_5,                         KC_6,    KC_7,           KC_8,           KC_9,           KC_0,         KC_MINS,
-  KC_TAB,  KC_Q,          KC_W,           KC_E,           KC_R,           KC_T,                         KC_Y,    KC_U,           KC_I,           KC_O,           KC_P,         KC_LBRC,
-  KC_ESC,  KC_A,          KC_S,           KC_D,           KC_F,           KC_G,                         KC_H,    KC_J,           KC_K,           KC_L,           KC_SCLN,      KC_QUOT,
-  KC_LSFT, KC_Z,          KC_X,           KC_C,           KC_V,           KC_B, KC_MUTE,       XXXXXXX, KC_N,    KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,      KC_RSFT,
-           LSG_T(KC_F15), LALT_T(KC_F16) ,LCTL_T(KC_F17), LGUI_T(KC_F18), LT(_LOWER,KC_F19), LT(_LOWER, KC_F20), RGUI_T(KC_F21), RCTL_T(KC_F22), RALT_T(KC_F23), C_S_T(KC_F24)
+KC_GRV,         KC_1,          KC_2,           KC_3,           KC_4,           KC_5,                            KC_6,    KC_7,           KC_8,           KC_9,           KC_0,         KC_MINS,
+KC_TAB,         KC_Q,          KC_W,           KC_E,           KC_R,           KC_T,                            KC_Y,    KC_U,           KC_I,           KC_O,           KC_P,         KC_LBRC,
+KC_ESC,         KC_A,          KC_S,           KC_D,           KC_F,           KC_G,                            KC_H,    KC_J,           KC_K,           KC_L,           KC_SCLN,      KC_QUOT,
+LSFT_T(KC_F13), KC_Z,          KC_X,           KC_C,           KC_V,           KC_B, KC_MUTE,          XXXXXXX, KC_N,    KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,      RSFT_T(KC_F24),
+                LSG_T(KC_F14), LALT_T(KC_F15) ,LCTL_T(KC_F16), LGUI_T(KC_F17), LT(_LOWER,KC_F18),    LT(_LOWER, KC_F19), RGUI_T(KC_F20), RCTL_T(KC_F21), RALT_T(KC_F22), C_S_T(KC_F23)
+),
+/* LOWER
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | F12  |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | Tab  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   |  |
+ * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
+ * | Shift|  =   |  -   |  +   |   {  |   }  |-------|    |-------|   [  |   ]  |   ;  |   :  |   \  | Shift|
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
+ *            |      |      |      |      |/       /         \      \ |      |      |      |      |
+ *            `----------------------------------'           '------''---------------------------'
+ */
+[_LOWER] = LAYOUT(
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,
+  _______, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_4, KC_KP_5,                         KC_KP_6, KC_KP_7, KC_KP_8, KC_KP_9,  KC_KP_0, KC_F12,
+  KC_CAPS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,                         KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,
+  KC_F13,  KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, _______,       _______, _______, _______, _______, _______,  _______, KC_F24,
+                    KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,        KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23
 ),
 /*
  * COLEMAK
@@ -66,27 +87,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                      KC_H,    KC_N,    KC_E,    KC_I,    KC_O,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,      XXXXXXX,KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
                  KC_LGUI,KC_LALT,KC_LCTL,MO(_LOWER), KC_ENT,        KC_SPC,  MO(_RAISE), KC_RCTL, KC_RALT, KC_RGUI
-),
-/* LOWER
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | F12  |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   |  |
- * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
- * | Shift|  =   |  -   |  +   |   {  |   }  |-------|    |-------|   [  |   ]  |   ;  |   :  |   \  | Shift|
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
- *            |      |      |      |      |/       /         \      \ |      |      |      |      |
- *            `----------------------------------'           '------''---------------------------'
- */
-[_LOWER] = LAYOUT(
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,
-  _______, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_4, KC_KP_5,                         KC_KP_6, KC_KP_7, KC_KP_8, KC_KP_9,  KC_KP_0, KC_F12,
-  KC_CAPS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,                         KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,
-  _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, _______,       _______, _______, _______, _______, _______,  _______, _______,
-                    _______, _______, _______, _______, KC_SPC,        KC_BSPC, _______, _______, _______, _______
 ),
 /* RAISE
  * ,----------------------------------------.                    ,-----------------------------------------.
